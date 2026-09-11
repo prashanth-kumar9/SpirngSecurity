e908308da520c5ee617a90bd2186eba5f245f471
prashanth-kumar9 <prashanthkornu9@gmail.com>
2026-09-11 09:42:01 +0530

    delete trash and added spring security

---
+++ src/main/java/com/springboot/cruddemo/security/UserManagerConfig.java
@@ -0,0 +1,72 @@
+package com.springboot.cruddemo.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class UserManagerConfig {
+
+	
+	
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+  /*  @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+*/
+
+	@Bean
+	public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+		
+		http.authorizeHttpRequests(configurer ->
+				configurer
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+				);
+		
+		//USES BASIC AUTH
+		
+		http.httpBasic(Customizer.withDefaults());
+		
+		//disable CSRF
+		http.csrf(csrf -> csrf.disable());
+		return http.build();
+	}
+
+}
---
+++ src/main/java/com/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
